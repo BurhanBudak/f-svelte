e0dfd261116e09096317c9198479a538e4ccff8c
Burhan Budak <budak.burhan@gmail.com>
2021-09-17 23:30:04 +0200

    second

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+	import { Route, Router } from "svelte-routing";
+	import Detail from "./detail/Detail.svelte";
+	import Library  from "./library/Library.svelte";
+	import Create from "./create/Create.svelte";
+	export let url = ''
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	main{
+		padding: var(--spacingLarge)
 	}
 </style>
+
+<Router {url}>
+<main>
+	<Route path="/create">
+		<Create />
+	</Route>
+	<Route path="/books/:id" let:params>
+		<Detail id={params.id}/>
+	</Route>
+	<Route path="/">
+		<Library />
+	</Route>
+</main>
+</Router>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import App from './App.svelte';
 
 const app = new App({
 	target: document.body,
+<<<<<<< HEAD
 	props: {
 		name: 'world'
 	}
+=======
+	// props: {
+	// 	name: 'world'
+	// }
+>>>>>>> 8fbc76b (first commit)
 });
 
 export default app;
